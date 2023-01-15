9f59c1cdb0129a60c7464a93491acd4dd4b8b1df
Eric Cornelissen <ericornelissen@gmail.com>
2023-01-15 23:27:49 +0100

    Add missing curly braces in shell scripts

--- validate.sh
+++ validate.sh
@@ -4,14 +4,14 @@ RESULT=$( \
 	curl --silent \
 		--output "/dev/null" \
 		--write-out "%{http_code}" \
-		--data-binary "@./$FILE" \
+		--data-binary "@./${FILE}" \
 		https://codecov.io/validate \
 )
 
-echo "status-code=$RESULT" >> "$GITHUB_OUTPUT"
+echo "status-code=${RESULT}" >> "${GITHUB_OUTPUT}"
 
-if [ "$RESULT" != "200" ]; then
-	echo "Codecov configuration is invalid (got $RESULT)"
+if [ "${RESULT}" != "200" ]; then
+	echo "Codecov configuration is invalid (got ${RESULT})"
 	exit 1
 else
 	echo "Codecov configuration is valid"
