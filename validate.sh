06e5b5f4e06c3d52bf064b628bfc567b855e5105
Eric Cornelissen <ericornelissen@gmail.com>
2023-03-02 11:36:01 +0100

    Format `validate.sh`

--- validate.sh
+++ validate.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-RESULT=$( \
+RESULT=$(
 	curl --silent \
 		--output "/dev/null" \
 		--write-out "%{http_code}" \
 		--data-binary "@./${FILE}" \
-		https://codecov.io/validate \
+		https://codecov.io/validate
 )
 
-echo "status-code=${RESULT}" >> "${GITHUB_OUTPUT}"
+echo "status-code=${RESULT}" >>"${GITHUB_OUTPUT}"
 
 if [ "${RESULT}" != "200" ]; then
 	echo "Codecov configuration is invalid (got ${RESULT})"
