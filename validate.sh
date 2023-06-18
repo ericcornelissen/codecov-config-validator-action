7474fcf17df3a47068f30bd146c25e7927ebf0ce
Eric Cornelissen <ericornelissen@gmail.com>
2023-06-18 14:43:07 +0200

    Helper functions for shell script (#40)
    * Add & use `info` logging helper.
    * Add & use helpers for starting and ending a log group.
    * Add & use helpers for setting an output value.

--- validate.sh
+++ validate.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+info() {
+	echo "$1"
+}
+
 debug() {
 	echo "::debug::$1"
+}
+
+group_start() {
+	echo "::group::$1"
+}
+
+group_end() {
+	echo "::endgroup::"
+}
+
+set_output() {
+	echo "$1=$2" >>"${GITHUB_OUTPUT}"
 }
 
 # ---------------------------------------------------------------------------- #
@@ -19,34 +35,34 @@ mapfile -t RESPONSE_LINES <<<"${RESPONSE}"
 
 debug 'Extracting response code from response'
 RESPONSE_CODE=${RESPONSE_LINES[-1]}
-echo "status-code=${RESPONSE_CODE}" >>"${GITHUB_OUTPUT}"
+set_output 'status-code' "${RESPONSE_CODE}"
 
 debug 'Logging response body'
-echo '::group::Codecov API response'
+group_start 'Codecov API response'
 for line in "${RESPONSE_LINES[@]::${#RESPONSE_LINES[@]}-1}"; do
-	echo "${line}"
+	info "${line}"
 done
-echo '::endgroup::'
+group_end
 
 debug 'Evaluating result'
 if [ "${RESPONSE_CODE}" == '200' ]; then
-	echo 'Codecov configuration is valid.'
+	info 'Codecov configuration is valid.'
 	exit 0
 elif [ "${RESPONSE_CODE}" -ge '400' ] && [ "${RESPONSE_CODE}" -lt '500' ]; then
-	echo "Codecov configuration is invalid (got ${RESPONSE_CODE})."
-	echo ''
-	echo "Update the Codecov configuration file at ${FILE} to make it valid."
+	info "Codecov configuration is invalid (got ${RESPONSE_CODE})."
+	info ''
+	info "Update the Codecov configuration file at ${FILE} to make it valid."
 	exit 1
 elif [ "${RESPONSE_CODE}" -ge '500' ]; then
-	echo "Codecov configuration could not be validated (got ${RESPONSE_CODE})."
-	echo ''
-	echo 'You can try to rerun this job after a short delay and it should pass then.'
-	echo "If the exact response code ${RESPONSE_CODE} persists, verify Codecov does not have an outage."
+	info "Codecov configuration could not be validated (got ${RESPONSE_CODE})."
+	info ''
+	info 'You can try to rerun this job after a short delay and it should pass then.'
+	info "If the exact response code ${RESPONSE_CODE} persists, verify Codecov does not have an outage."
 	exit 1
 else
-	echo "Codecov configuration validation state unknown (got ${RESPONSE_CODE})."
-	echo ''
-	echo 'If this persists open an issue at:'
-	echo 'https://github.com/ericcornelissen/codecov-config-validator-action/issues/new'
+	info "Codecov configuration validation state unknown (got ${RESPONSE_CODE})."
+	info ''
+	info 'If this persists open an issue at:'
+	info 'https://github.com/ericcornelissen/codecov-config-validator-action/issues/new'
 	exit 1
 fi
