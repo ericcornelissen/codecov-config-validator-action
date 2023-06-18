d807a0f2f30c3f7ee113b97f5432648d2e36367e
Eric Cornelissen <ericornelissen@gmail.com>
2023-06-18 14:27:51 +0200

    Add & use `info` logging helper
    Like `debug` but for log messages that inform user.

--- validate.sh
+++ validate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+info() {
+	echo "$1"
+}
+
 debug() {
 	echo "::debug::$1"
 }
@@ -24,29 +28,29 @@ echo "status-code=${RESPONSE_CODE}" >>"${GITHUB_OUTPUT}"
 debug 'Logging response body'
 echo '::group::Codecov API response'
 for t in "${RESPONSE_LINES[@]::${#RESPONSE_LINES[@]}-1}"; do
-	echo "${t}"
+	info "${t}"
 done
 echo '::endgroup::'
 
 debug 'Evaluating result'
 if [ "${RESPONSE_CODE}" == "200" ]; then
-	echo "Codecov configuration is valid."
+	info "Codecov configuration is valid."
 	exit 0
 elif [ "${RESPONSE_CODE}" -ge "400" ] && [ "${RESPONSE_CODE}" -lt "500" ]; then
-	echo "Codecov configuration is invalid (got ${RESPONSE_CODE})."
-	echo ''
-	echo "Update the Codecov configuration file at ${FILE} to make it valid."
+	info "Codecov configuration is invalid (got ${RESPONSE_CODE})."
+	info ''
+	info "Update the Codecov configuration file at ${FILE} to make it valid."
 	exit 1
 elif [ "${RESPONSE_CODE}" -ge "500" ]; then
-	echo "Codecov configuration could not be validated (got ${RESPONSE_CODE})."
-	echo ''
-	echo "You can try to rerun this job after a short delay and it should pass then."
-	echo "If the exact response code ${RESPONSE_CODE} persists, verify Codecov does not have an outage."
+	info "Codecov configuration could not be validated (got ${RESPONSE_CODE})."
+	info ''
+	info "You can try to rerun this job after a short delay and it should pass then."
+	info "If the exact response code ${RESPONSE_CODE} persists, verify Codecov does not have an outage."
 	exit 1
 else
-	echo "Codecov configuration validation state unknown (got ${RESPONSE_CODE})."
-	echo ''
-	echo 'If this persists open an issue at:'
-	echo 'https://github.com/ericcornelissen/codecov-config-validator-action/issues/new'
+	info "Codecov configuration validation state unknown (got ${RESPONSE_CODE})."
+	info ''
+	info 'If this persists open an issue at:'
+	info 'https://github.com/ericcornelissen/codecov-config-validator-action/issues/new'
 	exit 1
 fi
